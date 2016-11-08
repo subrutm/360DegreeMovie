9493a221bbd1f039565741248bd478f03d1eeff6
unknown <tmatsuoka@PD0529.bjn.brycen.co.jp>
2016-11-08 15:58:55 +0900

    first commit

---
+++ Assets/Scripts/HeadLookUpDown.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadLookUpDown : MonoBehaviour {
+    public float velocity = 0.7f;
+    public float maxHeight = 20.0f;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Debug.Log("HeadLookUpDown::Update");
+
+        float MoveY = 1.0f * velocity * Time.deltaTime;
+        float newY = transform.localPosition.y + MoveY;
+
+        if (newY >= 0f && newY < maxHeight) {
+            Camera.main.transform.localPosition = new Vector3(transform.position.x, newY, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    public Vector3 rotateRate;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rotateRate * Time.deltaTime);
+    }
+}
